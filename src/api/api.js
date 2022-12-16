fb38d4b2d69c8e7530986953a201d6c1685c1f27
Karine Pereira <karinesp89@gmail.com>
2022-12-16 13:52:52 +0100

    url production for deploy

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:8080/api/1.0",
-  production: "LINK DA SUA API DEPLOYADA AQUI!",
+  production: "https://deploy-artistacon.cyclic.app",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
