45b68949a2eaf9d809cad7baca4f51d505cc53fa
Karine Pereira <karinesp89@gmail.com>
2023-01-20 17:11:08 -0300

    add collapsed menu for small screns

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import logo from "../assets/images/artistacon-logo-light.svg";
 import { Link, useLocation } from "react-router-dom";
 import { AuthContext } from "../contexts/authContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { Menu } from "react-feather";
+import { Heart } from "react-feather";
 
 const noNavibarRoutes = ["/cadastro", "/login", "/cadastro/artista"];
 
@@ -9,34 +11,38 @@ export function Header() {
 
    const { pathname } = useLocation();
    const { loggedInUser } = useContext(AuthContext);
+   const [showMenu, setShowMenu] = useState(false);
+
+   function handleMenu() {
+      setShowMenu(!showMenu);
+   }
 
    if (noNavibarRoutes.some((item) => pathname.includes(item))) return null;
 
    return (
       <nav className="navbar navbar-expand-lg header">
-         <div className="container header__container justify-content-between">
+         <div className="container header__container d-flex">
 
-            <Link to="/" className="navbar-brand header__logo">
-               <img src={logo} alt="Artista Con" />
-            </Link>
+            <div className="flex-fill d-flex justify-content-between">
+               <Link to="/" className=" header__logo">
+                  <img className="img-fluid" src={logo} alt="Artista Con" />
+               </Link>
 
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-               <span className="navbar-toggler-icon"></span>
-            </button>
+               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" onClick={handleMenu} aria-label="Toggle navigation">
+                  <Menu />
+               </button>
+            </div>
 
-            <div className="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01">
-               <ul className="navbar__menu navbar-nav d-flex">
+            <div className={`${showMenu ? 'show' : ''} flex-lg-fill collapse navbar-collapse justify-content-between`}>
+
+               <ul className="navbar__menu navbar-nav d-flex mb-4 mb-lg-0">
                   <li className="nav-item"><Link to="/artistas">Artistas</Link></li>
                   <li className="nav-item"><Link to="/programacao">Programação</Link></li>
                   <li className="nav-item"><Link to="/sobre">Sobre</Link></li>
                </ul>
-            </div>
 
-            <div className="collapse navbar-collapse flex-grow-0 flex-shrink-0" id="navbarTogglerDemo01">
-
-               <ul className="navbar__links navbar-nav d-flex align-items-center">
+               <ul className="navbar__links navbar-nav d-flex align-items-lg-center">
                   {loggedInUser ?
-
                      <li className="nav-item"><Link to="/minha-conta/perfil">Olá {loggedInUser.user.name}</Link></li>
                      : (
                         <>
@@ -45,12 +51,17 @@ export function Header() {
                         </>
                      )
                   }
-
-                  <li><Link className="navbar__icon" to="minha-conta/favoritos">Favoritos</Link></li>
+                  <li>
+                     <Link className="navbar__favorites" to="minha-conta/favoritos">
+                        <Heart />
+                        <span className="d-lg-none">Meus Favoritos</span>
+                     </Link>
+                  </li>
                </ul>
-
             </div>
+
          </div>
+
       </nav>
    );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ export function Hero() {
          <div className="container">
             <div className="row align-items-center">
 
-               <div className="col-lg-6 d-flex flex-column gap-4 w-lg-50 align-items-start order-2 order-lg-1">
+               <div className="col-lg-6 d-flex flex-column gap-2 gap-lg-4 w-lg-50 align-items-start mb-4 mb-lg-0">
                   <div className="hero__text">
                      <p className="pre-title fw-bold mb-3">3, 4 e 5 de Setembro de 2023</p>
                      <h1 className="display-1 fw-bold mb-3">
@@ -20,7 +20,7 @@ export function Hero() {
                   <Link to="/" className="btn btn-lg btn-primary">Saiba mais</Link>
                </div>
 
-               <div className="col-lg-6 order-1 order-lg-2 hero__image">
+               <div className="col-lg-6 hero__image">
                   <img src={heroImge} alt="" />
                </div>
             </div>
